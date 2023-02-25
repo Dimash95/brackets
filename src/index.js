a2ef6dd392dc6561bc0f3a42e18ccdff85cd9e5e
Dimash <dinmukhamed.amirov@gmail.com>
2023-02-26 00:25:27 +0600

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openBrackets = new Set(bracketsConfig.map(config => config[0]));
+  const closeBrackets = new Set(bracketsConfig.map(config => config[1]));
+  const matchingBrackets = {};
+
+  for (const config of bracketsConfig) {
+    matchingBrackets[config[1]] = config[0];
+  }
+
+  for (const bracket of str) {
+    if (openBrackets.has(bracket)) {
+      stack.push(bracket);
+    } else if (closeBrackets.has(bracket)) {
+      const matchingBracket = matchingBrackets[bracket];
+      if (stack.length === 0 || stack.pop() !== matchingBracket) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 }
+
